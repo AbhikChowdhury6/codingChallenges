5b0b206a9c21f92b7d4cafd540015624ea092e28
AbhikChowdhury6 <abhik.ch6@gmail.com>
2024-05-09 09:41:40 -0700

    figured out a multi step combination problem

---
+++ pythonHackerrank/itertools/comboProblem.py
@@ -0,0 +1,27 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from itertools import combinations
+
+N = int(input())
+
+lis = input().split()
+
+k = int(input())
+
+numA = lis.count("a")
+
+# N choose k - (N - N choose numA) / N choose k
+
+lennCk = len(list(combinations(lis, k)))
+
+#print(lennCk)
+
+lenNumACk = len(list(combinations(range(N - numA), k)))
+
+
+print((lennCk - lenNumACk) / lennCk)
+
+
+
+
+
+
