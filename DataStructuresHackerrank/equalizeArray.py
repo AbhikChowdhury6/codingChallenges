943cf47fbc7df9ee564c04be6fe0901267220dd0
AbhikChowdhury6 <abhik.ch6@gmail.com>
2024-05-15 12:16:07 -0700

    did a couple more DS problems

---
+++ DataStructuresHackerrank/equalizeArray.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+
+#
+# Complete the 'equalizeArray' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def equalizeArray(arr):
+    # Write your code here
+    #the number with the largest number of recurrences
+    nc = Counter(arr)
+    return len(arr) - max(nc[x] for x in nc.elements())
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = equalizeArray(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
