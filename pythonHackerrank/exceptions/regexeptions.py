a15840ff204112cce10a585efee0f1f6caac96d7
AbhikChowdhury6 <abhik.ch6@gmail.com>
2024-05-15 12:05:46 -0700

    did a couple of DS problems

---
+++ pythonHackerrank/exceptions/regexeptions.py
@@ -0,0 +1,11 @@
+import re
+
+n = int(input())
+
+for _ in range(n):
+    try:
+        re.compile(input())
+        print(True)
+    except re.error:
+        print(False)
+        #print(re.compile
