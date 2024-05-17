8f03c4dab7e1332a1a8b2107c3ae9889ef4fa791
AbhikChowdhury6 <abhik.ch6@gmail.com>
2024-05-17 00:00:23 -0700

    solved zip didn't use zip

---
+++ pythonHackerrank/zip.py
@@ -0,0 +1,9 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+N, X = map(int, input().split())
+
+l = []
+for _ in range(X):
+    l.append(list(map(float, input().split())))
+
+print("\n".join([str(sum([x[i] for x in l])/X) for i in range(N)]))
