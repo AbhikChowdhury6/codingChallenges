9b3a5a77759dce88040635ee3b01c1ff46350e14
AbhikChowdhury6 <abhik.ch6@gmail.com>
2024-05-29 11:05:06 -0700

    did the ice cream parlor problem in 7:04, while the solution is triple indented, since they are each one line, I think this this the most readable way to write it

---
+++ AlgoHackerrank/search/iceCreamParlor.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'icecreamParlor' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER m
+#  2. INTEGER_ARRAY arr
+#
+
+def icecreamParlor(m, arr):
+    # Write your code here
+    for price1i in range(len(arr)-1):
+        for price2i in range(price1i + 1, len(arr)):
+            if arr[price1i] + arr[price2i] == m:
+                return sorted([price1i + 1, price2i + 1])
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        m = int(input().strip())
+
+        n = int(input().strip())
+
+        arr = list(map(int, input().rstrip().split()))
+
+        result = icecreamParlor(m, arr)
+
+        fptr.write(' '.join(map(str, result)))
+        fptr.write('\n')
+
+    fptr.close()
