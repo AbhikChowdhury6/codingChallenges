399607bff54e9866524f28ee9915cc1ba93cff03
AbhikChowdhury6 <abhik.ch6@gmail.com>
2024-06-11 18:59:39 -0700

    starting to put my interview prep problems on here for learning reference

---
+++ interviewQuestions/aceTheDSInterview/probaility/q6.py
@@ -0,0 +1,13 @@
+"""
+P(TP) = 0.001
+P(not TP) = 0.999
+P(posT |TP) = 0.98
+P(posT |FP) = 0.01
+P(TP |posT) = P(TP)P(postT |TP)/ P(posT) = abour 9%
+
+
+P(posT) = P(TP)P(posT |TP) + P(not TP)P(posT |not TP)
+
+
+
+"""
