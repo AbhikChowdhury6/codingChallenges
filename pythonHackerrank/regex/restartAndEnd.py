9e19a250c7eb5311cc3d040191d54123209aecd9
AbhikChowdhury6 <abhik.ch6@gmail.com>
2024-06-19 00:34:16 -0700

    properly did the start and end problem

--- pythonHackerrank/regex/restartAndEnd.py
+++ pythonHackerrank/regex/restartAndEnd.py
@@ -4,6 +4,16 @@ import re
 s = input()
 k = input()
 
-print(re.findall(r"{}".format(k), s))
-print(re.search(r"{}".format(k), s))
-print(re.search(r"{}".format(k), s))
+m = re.search(r"{}".format(k), s)
+
+lastStart = -1
+
+if m is None:
+    print("(-1,-1)")
+else:
+    for i in range(len(s)):
+        m = re.search(r"{}".format(k), s[i:])
+        if m is not None and m.start() + i != lastStart:
+            print(f"({m.start()+i}, {m.end()+i-1})")
+            lastStart = m.start() + i
+            
