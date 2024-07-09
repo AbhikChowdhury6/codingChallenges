a47e092dd1c9405fed67400f99db84b57d716e5b
AbhikChowdhury6 <abhik.ch6@gmail.com>
2024-07-08 22:27:01 -0700

    finished Trends kaggle exercise

--- kaggle/courses/timeSeries/trend.py
+++ kaggle/courses/timeSeries/trend.py
@@ -22,12 +22,14 @@ dtype = {
     'sales': 'float32',
     'onpromotion': 'uint64',
 }
+
 store_sales = pd.read_csv(
     comp_dir / 'train.csv',
     dtype=dtype,
     parse_dates=['date'],
     infer_datetime_format=True,
 )
+
 store_sales = store_sales.set_index('date').to_period('D')
 store_sales = store_sales.set_index(['store_nbr', 'family'], append=True)
 average_sales = store_sales.groupby('date').mean()['sales']
@@ -50,5 +52,45 @@ q_1.check()
 ax = food_sales.plot(**plot_params, alpha=0.5)
 ax = trend.plot(ax=ax, linewidth=3)
 
+
+trend = average_sales.rolling(
+    window=365,
+    center=True,
+    min_periods=183,
+).mean()
+
+ax = average_sales.plot(**plot_params, alpha=0.5)
+ax = trend.plot(ax=ax, linewidth=3)
+
+
+from statsmodels.tsa.deterministic import DeterministicProcess
+
+y = average_sales.copy()  # the target
+
+# YOUR CODE HERE: Instantiate `DeterministicProcess` with arguments
+# appropriate for a cubic trend model
+dp = DeterministicProcess(
+    index=y.index,
+    order=3,
+    drop=True
+)
+
+# YOUR CODE HERE: Create the feature set for the dates given in y.index
+X = dp.in_sample()
+
+# YOUR CODE HERE: Create features for a 90-day forecast.
+X_fore = dp.out_of_sample(steps=90)
+
+
+model = LinearRegression()
+model.fit(X, y)
+
+y_pred = pd.Series(model.predict(X), index=X.index)
+y_fore = pd.Series(model.predict(X_fore), index=X_fore.index)
+
+ax = y.plot(**plot_params, alpha=0.5, title="Average Sales", ylabel="items sold")
+ax = y_pred.plot(ax=ax, linewidth=3, label="Trend", color='C0')
+ax = y_fore.plot(ax=ax, linewidth=3, label="Trend Forecast", color='C3')
+ax.legend();
 
 
