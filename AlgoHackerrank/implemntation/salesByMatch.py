6a885f4c390a4e674fbb5d8b68159e3946eed33d
AbhikChowdhury6 <abhik.ch6@gmail.com>
2025-02-07 15:48:19 -0700

    first coding problem in a while, easing in

---
+++ AlgoHackerrank/implemntation/salesByMatch.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    # Write your code here
+    # add each element to a dictonary keyed by value
+    color_counts = {k:0 for k in set(ar)}
+    for element in ar:
+        color_counts[element] += 1
+    return sum([x//2 for x in list(color_counts.values())])
+    
+    
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
