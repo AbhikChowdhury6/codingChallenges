6f95ba05244413b93e82ad63c80de0bab020f3d9
AbhikChowdhury6 <abhik.ch6@gmail.com>
2025-03-20 22:10:03 -0700

    cleaned up longest substring

--- leetcode/lisngestSubstring.py
+++ leetcode/lisngestSubstring.py
@@ -1,20 +1,15 @@
 def lengthOfLongestSubstring(s: str) -> int:
-    #keep adding to a set and reset at the index of the repeated letter everytime you encounter one
     if len(s) == 0: return 0
     candidate_start = 0
-    curr_index = 0
     max_len = 1
-    for i in range(0, len(s) - 1):
-        print(f"candidate_start: {candidate_start}")
-        sublist = s[candidate_start:i + 1]
-        print(sublist)
-        if s[i+1] in sublist:
-            candidate_start += sublist.find(s[i+1]) + 1
+    for i in range(len(s)-1):
+        sublist = s[candidate_start:i+1]
+        new_letter = s[i+1]
+        if new_letter in sublist:
+            candidate_start += sublist.find(new_letter) + 1
         elif max_len < len(sublist) + 1:
                 max_len = len(sublist) + 1
-
-    
-    return max_len
+    return max_len  
 
 
 print(lengthOfLongestSubstring("pwwkew"))
