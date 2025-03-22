3d3100e2f104ec1d083d9bbac9db05ffede61974
AbhikChowdhury6 <abhik.ch6@gmail.com>
2025-03-21 20:01:26 -0700

    did zig zag conv once, but is probably trivial using some slicing

--- leetcode/6zigzagConv.py
+++ leetcode/6zigzagConv.py
@@ -6,19 +6,40 @@ def conv(s,num_rows):
         next_idx = start_idx
         while next_idx < len(s):
             first_row.append(s[next_idx])
-            next_idx += (2*num_rows) - 2
+            next_idx += (2*num_rows) - 2 if num_rows > 1 else 1
         return "".join(first_row)
     
+    def get_mid_row(s, num_rows, row_num):
+        this_row = []
+        next_idx = row_num
+        short_jump = 2*row_num
+        long_jump = (2*num_rows - 2) - short_jump
+        
+        while next_idx < len(s):
+            this_row.append(s[next_idx])
+            next_idx  += long_jump
+            if next_idx >= len(s):
+                break
+            this_row.append(s[next_idx])
+            next_idx  += short_jump
+        return "".join(this_row)
+        
     
-    
-    firstRow = get_forl_row(s, num_rows, num_rows-1)
+    firstRow = get_forl_row(s, num_rows, 0)
     str_to_return = firstRow
-    
-    
-    
-    lastRow = get_forl_row(s, num_rows, num_rows-1)
-    return get_forl_row(s, num_rows, num_rows-1)
+
+    if num_rows == 1:
+        return str_to_return
+
+    for row_num in range(1, num_rows - 1):
+        next_row = get_mid_row(s, num_rows, row_num)
+        str_to_return += next_row
+
+    last_row = get_forl_row(s, num_rows, num_rows-1)
+    str_to_return += last_row
+    return str_to_return
     
 
 
-print(conv("PAYPALISHIRING", 3))
+print(conv("A", 1))
+print("PAHNAPLSIIGYIR")
