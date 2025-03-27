decb40d3c192ee8e16aa7e9a71fcdc2a6108e90f
AbhikChowdhury6 <abhik.ch6@gmail.com>
2025-03-26 22:32:42 -0700

    did the buy and sell problem, but will review it

--- neetCode150/buyAndSellStock.py
+++ neetCode150/buyAndSellStock.py
@@ -1,15 +1,21 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        derivitives =  [prices[i+1] - prices[i] for i in range(len(prices)-1)]
-        return max([])
-        curr_valley_idx = 0
-        curr_peak_idx = len(prices) - 1
-        max_return = 0
-        while curr_valley_idx < curr_peak_idx:
-            #calc next valley idx
+        # new plan
+        # two pointers at the beginning
+        # the first one is for the lowest low so far
+        # the next one is for the highest high so far
 
-            #calc prev peak idx
+        #there is only the opportunity to make more money if 
+        # the right pointer finds a lower low
+        # if it does, update our low pointer
+        # keep a running max since the lowest low might not be followed by the max profit
+        low_i = 0
+        max_profit = 0
+        for curr_i in range(1, len(prices)):
+            if prices[curr_i] - prices[low_i] > max_profit:
+                max_profit = prices[curr_i] - prices[low_i] 
             
-            #if the condition is met
-            # move either or both 
-            pass
+            if prices[curr_i] < prices[low_i]:
+                low_i = curr_i 
+        return max_profit
+
