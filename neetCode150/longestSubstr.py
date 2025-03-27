5bd8039d5ab610b5d929d42d41adeba5c3f3a77f
AbhikChowdhury6 <abhik.ch6@gmail.com>
2025-03-26 22:51:48 -0700

    dude that was so much easier than the last one, I did still have to debug my list indexing though, didn't use print statemnts, but would like to be ablt to not make those off by one errors

---
+++ neetCode150/longestSubstr.py
@@ -0,0 +1,17 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if len(s) == 0: return 0
+        curr_max_len = 1
+        sublist_cutoff = 0
+        for i in range(1, len(s)):
+            new_char = s[i]
+            sublist = s[sublist_cutoff:i]
+            if new_char in sublist:
+                sublist_cutoff += sublist.index(new_char) + 1
+            
+            sublist_len = i - (sublist_cutoff-1)
+            if  sublist_len > curr_max_len:
+                curr_max_len = sublist_len
+
+        return curr_max_len
+        
