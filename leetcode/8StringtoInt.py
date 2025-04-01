ccd1cfccb5be41d004fb280d5aeeabe39064503c
AbhikChowdhury6 <abhik.ch6@gmail.com>
2025-04-01 00:10:06 -0700

    finally finished char replacemnet, i'll take a break and write some other code for a bit

--- leetcode/8StringtoInt.py
+++ leetcode/8StringtoInt.py
@@ -1,3 +1,10 @@
+# it would have been faster to actually read the problem statement
+# then I would have seen that we should have stopped at non nums
+# we also could have seen the + and the - and the free space being easy to handle
+# the cases I ended up using does seem pretty minimal
+# yeah takeaway, tackle requirements section by section
+
+
 class Solution:
     def myAtoi(self, s: str) -> int:
         newS = []
@@ -9,9 +16,9 @@ class Solution:
         elif s[0] == '+':
             newS.append('+')
             s = s[1:]
-        
+        #s = s.strip()
         i = 0
-        while i < len(s) and (s.strip()[i].isnumeric()):
+        while i < len(s) and s[i].isnumeric():
             newS.append(s[i])
             i += 1
         if len(newS) == 0 or (len(newS) == 1 and (newS[0] == '-' or newS[0] == '+')): return 0
--- neetCode150/charReplacement.py
+++ neetCode150/charReplacement.py
@@ -1,5 +1,54 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
+        #ok new plan
+        # once I hit k number of different charecters reset start i 
+        # move up star i until it saves me a change
+        #min num of diff chars has to = k
+        # intake the new char and move the start i up until you save on a switch
+        #should I keep a dict of the counts of each charecter
+        # and then sum the counts of every non max char
+        def getMaxKey(d):
+            return max(d.keys(), key = lambda x:d[x])
+        
+        def getNumMaxKeys(mk, d):
+            return len([x for x in d if d[x] == d[mk]])
+        
+        def numChanges(mk, d):
+            return sum(d.values()) - d[mk]
+        
+        substr_start_i = 0
+        max_len = 1
+        curr_letter_counts = {s[0]:1}
+        for i in range(1, len(s)):
+            # add the next letter to the string
+            if s[i] not in curr_letter_counts:
+                curr_letter_counts[s[i]] = 0
+            curr_letter_counts[s[i]] += 1
+            
+            max_curr_count_key = getMaxKey(curr_letter_counts)
+            nc = numChanges(max_curr_count_key, curr_letter_counts)
+
+            # check if it violates the condition
+            if nc > k:
+                #move up until removing s[substr_start_i] doesn't remove from the max letter
+                while True:
+                    # print(substr_start_i)
+                    letter_to_remove = s[substr_start_i]
+                    substr_start_i += 1
+                    curr_letter_counts[letter_to_remove] -= 1
+
+                    max_curr_count_key = getMaxKey(curr_letter_counts)
+                    num_of_those_keys = getNumMaxKeys(max_curr_count_key, curr_letter_counts)
+                    
+                    if letter_to_remove != max_curr_count_key and num_of_those_keys == 1:
+                        break
+            
+            # update the max_len
+            curr_substr_len = i - (substr_start_i-1)
+            max_len = max(max_len, curr_substr_len)
+        return max_len
+
+
         # we have a budget of k charecters 
         # keep on reading in chars into the sublist
         # if adding the next char exceeds the budget
